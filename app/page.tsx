52235d4294654891c332b6f9f566fcfd1d6dc141
Ibidapo Ayomide <ibidapoayomide754@gmail.com>
2024-11-15 18:21:36 +0100

    setup the lottery form

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,29 @@
 
 "use client"
 import lottery from "@/utils/lottery";
-import { useEffect, useState } from "react";
+import web3 from "@/utils/web3";
+import { FormEvent, useEffect, useState } from "react";
 
 export default function Home() {
 
   const [manager, setManager] = useState(null)
+  const [players, setPlayers] = useState([])
+  const [balance, setBalance] = useState(0)
+
+  const [ether, setEther] = useState("")
+  const [message, setMessage] = useState("")
 
   useEffect(() => {
 
     const fetch = async () => {
       try {
-        const m = await lottery.methods.manager().call()
-        const players = await lottery.methods.returnEntries().call()
-        console.log(players);
-        if (typeof m === "string") {
-          setManager(m)
+        const [m, player, accountBalance] = await Promise.allSettled([lottery.methods.manager().call(), lottery.methods.returnEntries().call(), web3.eth.getBalance(lottery.options.address!)])
+
+
+        setPlayers(player.value)
+        setBalance(accountBalance.value)
+        if (typeof m.value === "string") {
+          setManager(m.value)
         }
 
       } catch (error) {
@@ -27,10 +35,58 @@ export default function Home() {
     fetch()
   }, [])
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (!ether) {
+      alert("Please some value")
+      return
+    }
+
+    const accounts = await web3.eth.getAccounts()
+
+    setMessage("Waiting on transaction success...")
+    console.log(accounts);
+    
+    try {
+
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(ether, "ether")
+      })
+
+      setMessage("Transaction successful, you have been entered into the lottery")
+    } catch (error) {
+      if (error instanceof Error) {
+        setMessage(error.message.split(":")[2])
+      }
+    }
+
+
+
+ 
+
+  }
+
   return (
     <div className="w-full h-screen pt-10">
-      <h1 className="text-2xl text-center font-semibold">Welcome to the Lottery Game </h1>
-      {manager && <p className="text-sm text-gray-500 text-center">This contract was deployed by {manager}</p>}
+      <h1 className="text-2xl font-semibold">Welcome to the Lottery Game </h1>
+      {manager && <p className="text-sm text-gray-500">This contract was deployed by {manager}</p>}
+
+      <p className="text-gray-500">There are currently  {players.length} people entered, competing to win {web3.utils.fromWei(balance, "ether")} ether</p>
+
+      <div className="pt-10">
+        <form className="space-y-4" onSubmit={(e) => handleSubmit(e)}>
+          <h4>Want to try your luck?</h4>
+          <div className="flex flex-col space-y-2">
+            <label>Amount of ether  to enter</label>
+            <input className="bg-gray-100 p-3 rounded-md border-none hover:border-none outline-none hover:outline-none " type="text" value={ether} onChange={(e) => setEther(e.target.value)} />
+          </div>
+
+          <button className="bg-amber-500 font-medium px-5 py-2 rounded-md" type="submit">Enter</button>
+          {message && <p>{message}</p>}
+        </form>
+      </div>
     </div>
   );
 }
--- utils/lottery.ts
+++ utils/lottery.ts
@@ -1,77 +1,77 @@
 import web3 from "./web3";
 
-const address = "0xC41387eDaE7b8274308c2992923e16C43566E97c"
+const address = "0x59D86bE322c47074074fa100c01C1993C1b542cc"
 const abi = [
     {
-        "inputs": [],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
     },
     {
-        "inputs": [],
-        "name": "enter",
-        "outputs": [],
-        "stateMutability": "payable",
-        "type": "function"
+      "inputs": [],
+      "name": "enter",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
     },
     {
-        "inputs": [],
-        "name": "manager",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
+      "inputs": [],
+      "name": "manager",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
     },
     {
-        "inputs": [],
-        "name": "pickRandomWinner",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "function"
+      "inputs": [],
+      "name": "pickRandomWinner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
     },
     {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "players",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "players",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
     },
     {
-        "inputs": [],
-        "name": "returnEntries",
-        "outputs": [
-            {
-                "internalType": "address[]",
-                "name": "",
-                "type": "address[]"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
+      "inputs": [],
+      "name": "returnEntries",
+      "outputs": [
+        {
+          "internalType": "address[]",
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
     }
-]
+  ]
 
 export default new web3.eth.Contract(abi, address)
